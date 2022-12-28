b7ccad16807da6e2d7024492310527e95688051f
ptrkstr <patrickbdev@users.noreply.github.com>
2022-12-28 21:24:22 +0100

    Rewrite with new syringe format

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,7 +18,7 @@ let package = Package(
             targets: ["Injector"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/hmlongco/Resolver.git", from: "1.5.0")
+        .package(url: "https://github.com/hmlongco/Factory.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,7 +26,7 @@ let package = Package(
         .target(
             name: "Injector",
             dependencies: [
-                "Resolver"
+                "Factory"
             ]),
         .testTarget(
             name: "InjectorTests",

f09985d1d4eed1d2f054db15fe55999cf2ec378d
ptrkstr <ptrkstr@users.noreply.github.com>
2023-11-13 23:04:46 +0400

    Simplify tests

--- Tests/InjectorTests/InjectorTests.swift
+++ Tests/InjectorTests/InjectorTests.swift
@@ -1,60 +1,81 @@
 import XCTest
 @testable import Injector
 
-protocol PersonType {
-    var name: String { get set }
+protocol ObjectType {
+    var id: String { get set }
 }
-class Person: PersonType {
-    var name: String = ""
-}
-class Person_Mock: PersonType {
-    var name: String = ""
+class Object: ObjectType {
+    var id: String = ""
+
+    init(id: String) {
+        self.id = id
+    }
 }
 
 final class InjectorTests: XCTestCase {
 
     func test_clean() {
-        let medicine = Medicine<PersonType>(Person())
+        let medicine = Medicine<ObjectType>(Object(id: ""))
 
         var person = medicine.inject()
-        XCTAssertEqual(person.name, "")
-        person.name = "alex"
+        XCTAssertEqual(person.id, "")
+        person.id = "alex"
 
         person = medicine.inject()
-        XCTAssertEqual(person.name, "alex")
+        XCTAssertEqual(person.id, "alex")
         Syringe.clean()
 
         person = medicine.inject()
-        XCTAssertEqual(person.name, "")
+        XCTAssertEqual(person.id, "")
     }
 
     func test_environmentReal() throws {
-        let medicine = Medicine<PersonType>(Person(), environment: .real)
+        let medicine = Medicine<ObjectType>(Object(id: "real"), environment: .real)
         let person = medicine.inject()
-        XCTAssertTrue(person is Person)
+        XCTAssertEqual(person.id, "real")
     }
 
     func test_environmentUnitTest() throws {
-        let medicine = Medicine<PersonType>(Person(), mock: Person_Mock(), environment: .unitTest)
+        let medicine = Medicine<ObjectType>(Object(id: "real"), mock: Object(id: "mock"), environment: .unitTest)
         let person = medicine.inject()
-        XCTAssertTrue(person is Person_Mock)
+        XCTAssertEqual(person.id, "mock")
     }
 
     func test_environmentUiTest() throws {
-        let medicine = Medicine<PersonType>(Person(), mock: Person_Mock(), environment: .uiTest)
+        let medicine = Medicine<ObjectType>(Object(id: "real"), mock: Object(id: "mock"), environment: .uiTest)
         let person = medicine.inject()
-        XCTAssertTrue(person is Person_Mock)
+        XCTAssertEqual(person.id, "mock")
     }
 
     func test_environmentSwiftUIPreview() throws {
-        let medicine = Medicine<PersonType>(Person(), mock: Person_Mock(), environment: .swiftUIPreview)
+        let medicine = Medicine<ObjectType>(Object(id: "real"), mock: Object(id: "mock"), environment: .swiftUIPreview)
         let person = medicine.inject()
-        XCTAssertTrue(person is Person_Mock)
+        XCTAssertEqual(person.id, "mock")
+    }
+
+    func test_fullInitializer() throws {
+        let map: [InjectorEnvironment: ObjectType] = [
+            .unitTest: Object(id: "unitTest"),
+            .uiTest: Object(id: "uiTest"),
+            .swiftUIPreview: Object(id: "preview"),
+            .real: Object(id: "real")
+        ]
+        map.forEach { pair in
+            let medicine = Medicine<ObjectType>(
+                Object(id: "real"),
+                unitTests: Object(id: "unitTest"),
+                uiTests: Object(id: "uiTest"),
+                swiftUIPreview: Object(id: "preview"),
+                environment: pair.key
+            )
+            let person = medicine.inject()
+            XCTAssertEqual(person.id, pair.value.id)
+        }
     }
 
     func test_environmentResolve() throws {
-        let medicine = Medicine<PersonType>(Person(), mock: Person_Mock())
+        let medicine = Medicine<ObjectType>(Object(id: "real"), mock: Object(id: "mock"))
         let person = medicine.inject()
-        XCTAssertTrue(person is Person_Mock)
+        XCTAssertEqual(person.id, "mock")
     }
 }
